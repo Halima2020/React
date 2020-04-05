ac2f6178927c0d6fd9068fce443d54e624f92102
Halima Bala <halimasadiyabala@gmail.com>
2020-04-04 20:26:41 -0500

    Combining Reducers and Comment Form

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,6 +1,93 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, FormGroup, Label, Row, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { LocalForm, Control, Errors } from 'react-redux-form';
+
+
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
+class ComponentForm extends Component {
+    constructor(props) {
+    super(props);
+    this.state = {
+        firstName: '',
+        lastName: '',
+        isModalOpen: false,
+        };
+
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+    }
+    toggleModal(values){
+      this.setState({isModalOpen:!this.state.isModalOpen});
+    }
+
+    handleSubmit(values) {
+        console.log("Current state is: ", values);
+        alert("Current state is: " + JSON.stringify(values));
+    }
+
+    render() {
+        return(
+            <>
+            <Button onClick={this.toggleModal} color="secondary" outline><i className="fa fa-pencil"></i> Submit Comment</Button>
+            <Modal isOpen={this.state.isModalOpen}>
+                <ModalHeader>Submit Comment</ModalHeader>
+                <ModalBody>
+                        <LocalForm onSubmit={this.handleSubmit}>
+                            <Row className="form-group">
+                      <Label htmlFor="rating" md={4}>Rating</Label>
+                      <Col md={10}>
+                      <Control.select model=".rating" name="rating" className="form-control">
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </Control.select>
+                                     </Col>
+                            </Row>
+                            <Row className="form-group">
+                      <Label htmlFor="author" md={4}>Your Name</Label>
+                      <Col md={10}>
+                                    <Control.text model=".author" name="author"
+                                        placeholder="Your Name"
+                                        className="form-control"
+                                        validators={{
+                                            minLength: minLength(2),
+                                            maxLength: maxLength(15)
+                                        }}>
+                                    </Control.text>
+                                    <Errors
+                                        className="text-danger"
+                                        model=".author"
+                                        show="touched"
+                                        component="div"
+                                        messages={{
+                                            minLength: 'Must be at least 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />
+                                    </Col>
+                            </Row>
+                            <Row className="form-group">
+                      <Label htmlFor="text" md={4}>Comment</Label>
+                      <Col md={10}>
+                                    <Control.textarea model=".text" name=".text"
+                                        rows="12"
+                                        className="form-control">
+                                        </Control.textarea>
+                                        </Col>
+                            </Row>
+                            <Button type="submit" color="primary">Submit</Button>
+                  </LocalForm>
+                </ModalBody>
+            </Modal>
+            </>
+        );
+    }
+}
 
 function RenderCampsite({campsite}) {
         return(
@@ -23,6 +110,7 @@ function RenderCampsite({campsite}) {
                     {comments.map(comments => <div key={comments.id}>{comments.text}<br />
                        -- {comments.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comments.date)))}
                         </div> )}
+                        <ComponentForm />
                </div>
             );
         }
